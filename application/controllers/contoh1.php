ed635481cf62dd7985fb19d03533645c1976159b
766kalvin <kalvin766hi@gmail.com>
2022-09-28 19:02:29 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Kalvin Silalahi
+            saya tinggal di daerah sukaraja
+            olahraga yang saya suka adalah 
+            bulutangkis";
+    }
+}
